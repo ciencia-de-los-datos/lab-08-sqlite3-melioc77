f1cc708123973de6a074d4cd915492c09e6e2ef1
melioc77 <121962019+melioc77@users.noreply.github.com>
2024-05-03 17:25:58 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -292,17 +292,19 @@ def test_14():
     conn, _ = load_data()
     with open("pregunta_14.sql", encoding="utf-8") as file:
         query = file.read()
-    assert pd.read_sql_query(query, conn).to_dict() == {
+        resultado = pd.read_sql_query(query, conn).to_dict()
+        resultado['avg(c21)'][4] = round(resultado['avg(c21)'][4],2)
+    print(pd.read_sql_query(query, conn).to_dict())
+    assert resultado == {
         "K0": {0: "A", 1: "B", 2: "C", 3: "D", 4: "E"},
         "avg(c21)": {
-            0: 593.495,
+            0: 593.5,
             1: 575.47,
-            2: 530.7529999999999,
-            3: 655.6125,
-            4: 555.323076923077,
+            2: 530.75,
+            3: 655.61,
+            4: 555.32,
         },
     }
-
 
 test = {
     "01": test_01,
